e59c0c25729a1852bd4aa608b234addcddf38403
Denis Kalenga Watshipamba <deniskalenga94@gmail.com>
2023-05-07 16:52:50 +0200

    Removed all the cool stuff that I added but are actually not needed for this assignment. EG: JPA, Migration, MVC, testContaines, etc

--- src/main/resources/db/migration/V3__drop_author.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE books
-    ADD COLUMN author_name varchar(255);
-
-UPDATE books b SET author_name = (SELECT name FROM authors au WHERE au.id = b.author_id) WHERE b.author_name IS NULL;
-
-ALTER TABLE books
-DROP COLUMN author_id;
-
-DROP TABLE authors;
